abb4b7ebc49f361da59c1b11cd3974cba95153e8
Neolux Lee <neolux_lee@outlook.com>
2025-11-08 11:43:41 +0800

    Add 349. Intersection of Two Arrays (Python)

--- code/python/349_intersection_of_two_arrays.py
+++ code/python/349_intersection_of_two_arrays.py
@@ -2,6 +2,8 @@ from typing import List
 
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        set1 = set(nums1)
-        set2 = set(nums2)
-        return list(set1 & set2)
+        """
+        349. Intersection of Two Arrays
+        https://leetcode.com/problems/intersection-of-two-arrays/
+        """
+        return list(set(nums1) & set(nums2))
