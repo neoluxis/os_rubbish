1803272d2c133e2a5bb5be4c284abd49e0e17545
Neolux Lee <neolux_lee@outlook.com>
2025-11-08 09:43:28 +0800

    Add Python solution for LeetCode 349: Intersection of Two Arrays

--- code/python/349_intersection_of_two_arrays.py
+++ code/python/349_intersection_of_two_arrays.py
@@ -1,11 +1,7 @@
 from typing import List
 
-
 class Solution:
-    """
-    LeetCode 349. Intersection of Two Arrays
-    https://leetcode.com/problems/intersection-of-two-arrays/
-    """
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # 使用内置集合求交集，时间复杂度 O(n + m)，空间复杂度 O(n + m)
-        return list(set(nums1) & set(nums2))
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return list(set1 & set2)
